37a027af90aa608268aab39be9649884ea2426fe
凉快 <751747443@qq.com>
2017-10-29 23:09:26 +0800

    MyIterator

---
+++ collection/src/main/java/MyIterator.java
@@ -0,0 +1,16 @@
+/**
+ * 迭代器
+ *
+ * @param <E>
+ *
+ * @author liangkuai
+ * @date 2017/10/29
+ */
+public interface MyIterator<E> {
+
+    boolean hasNext();
+
+    E next();
+
+    void remove();
+}

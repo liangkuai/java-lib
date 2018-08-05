a3cc30c785de747dc07c7b9a8f1de8f8d7be8c86
凉快 <751747443@qq.com>
2018-08-05 17:55:43 +0800

    select sort
    modify

--- data-structure/src/main/java/sort/selection/SelectSort.java
+++ data-structure/src/main/java/sort/selection/SelectSort.java
@@ -10,12 +10,15 @@ public class SelectSort {
 
     public int[] sort(int[] origin) {
         for (int i = 0; i < origin.length; ++i) {
+
+            // 查找最小元素下标
             int minIndex = i;
             for (int j = i; j < origin.length; ++j) {
                 if (origin[j] < origin[minIndex])
                     minIndex = j;
             }
 
+            // 交换
             int temp;
             temp = origin[i];
             origin[i] = origin[minIndex];

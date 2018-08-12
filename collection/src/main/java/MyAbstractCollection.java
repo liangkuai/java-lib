c8294a842575190791749c73a095e2a417b4ded3
凉快 <751747443@qq.com>
2018-08-12 18:41:35 +0800

    AbstractCollection

---
+++ collection/src/main/java/MyAbstractCollection.java
@@ -0,0 +1,67 @@
+import java.util.Arrays;
+import java.util.Iterator;
+
+public abstract class MyAbstractCollection<E> implements MyCollection {
+
+    protected MyAbstractCollection() {
+    }
+
+    public abstract Iterator<E> iterator();
+
+    public abstract int size();
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public boolean contains(Object o) {
+        Iterator<E> it = iterator();
+        if (o == null) {
+            while (it.hasNext())
+                if (it.next() == null)
+                    return true;
+        } else {
+            while (it.hasNext())
+                if (o.equals(it.next()))
+                    return true;
+        }
+        return false;
+    }
+
+    public Object[]
+    toArray() {
+        Object[] r = new Object[size()];
+        Iterator<E> it = iterator();
+        for (int i = 0; i < r.length; i++) {
+            if (!it.hasNext())  // 元素为 null，结束转换
+                return Arrays.copyOf(r, i);
+            r[i] = it.next();
+        }
+        return it.hasNext() ? finishToArray(r, it) : r;
+    }
+
+    private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
+
+    private static <T> T[] finishToArray(T[] r, Iterator<?> it) {
+        int i = r.length;
+        while (it.hasNext()) {
+            int cap = r.length;
+            if (i == cap) {
+                int newCap = cap + (cap >> 1) + 1;
+                if (newCap - MAX_ARRAY_SIZE > 0)
+                    newCap = hugeCapacity(cap + 1);
+                r = Arrays.copyOf(r, newCap);
+            }
+            r[i++] = (T) it.next();
+        }
+        return (i == r.length) ? r : Arrays.copyOf(r, i);
+    }
+
+    private static int hugeCapacity(int minCapacity) {
+        if (minCapacity < 0)
+            throw new OutOfMemoryError("Required array size too large");
+        return (minCapacity > MAX_ARRAY_SIZE) ?
+                Integer.MAX_VALUE :
+                MAX_ARRAY_SIZE;
+    }
+}
---
+++ collection/src/main/java/MyCollection.java
@@ -0,0 +1,36 @@
+import java.util.Iterator;
+
+public interface MyCollection<E> extends Iterator<E> {
+
+    int size();
+
+    boolean isEmpty();
+
+    boolean contains();
+
+    Iterator<E> iterator();
+
+    Object[] toArray();
+
+    <T> T[] toArray(T[] a);
+
+    boolean add(E e);
+
+    boolean remove(Object o);
+
+    boolean containsAll(MyCollection<?> c);
+
+    boolean addAll(MyCollection<? extends E> c);
+
+    boolean removeAll(MyCollection<?> c);
+
+    boolean retainAll(MyCollection<?> c);
+
+    void clear();
+
+    @Override
+    boolean equals(Object o);
+
+    @Override
+    int hashCode();
+}

7e0697b0c13205bee1bbe9d2854d4629e8b28afc
凉快 <751747443@qq.com>
2018-09-01 16:25:25 +0800

    heap sort
    modify

---
+++ data-structure/src/main/java/heap/binary/BinaryHeap.java
@@ -0,0 +1,8 @@
+package heap.binary;
+
+/**
+ * @author liangkuai
+ * @date 2018/9/1
+ */
+public class BinaryHeap {
+}
--- data-structure/src/main/java/sort/heap/HeapSort.java
+++ data-structure/src/main/java/sort/heap/HeapSort.java
@@ -11,40 +11,40 @@ public class HeapSort {
     /**
      * 为了简化操作，便于理解；数组下标从 1 开始。
      */
-    public int[] sort(int[] origin) {
+    public int[] sort(int[] a) {
 
         // 升序排序，先构造最大堆
-        for (int i = (origin.length / 2); i > 0; --i) {
-            adjust(origin, i, origin.length - 1);
+        for (int i = (a.length / 2); i > 0; --i) {
+            adjust(a, i, a.length - 1);
         }
 
         // 循环输出堆顶到数组尾部，再调整堆
-        for (int i = origin.length - 1; i > 1; --i) {
-            int temp = origin[i];
-            origin[i] = origin[1];
-            origin[1] = temp;
+        for (int i = a.length - 1; i > 1; --i) {
+            int temp = a[i];
+            a[i] = a[1];
+            a[1] = temp;
 
-            adjust(origin, 1, i - 1);
+            adjust(a, 1, i - 1);
         }
 
-        return origin;
+        return a;
     }
 
-    public void adjust(int[] origin, int index, int len) {
-        int val = origin[index];
+    public void adjust(int[] a, int index, int len) {
+        int val = a[index];
         int i;
         for (i = index * 2; i <= len; i *= 2) {
             // index 的两个子节点比较
-            if ((i < len) && (origin[i] < origin[i + 1]))
+            if ((i < len) && (a[i] < a[i + 1]))
                 ++i;
             // index 与较大的子节点比较
-            if (val > origin[i])
+            if (val > a[i])
                 break;
             // index 节点取较大子节点的值
             // 再让 index 指向被交换值得子节点，继续往下调整堆
-            origin[index] = origin[i];
+            a[index] = a[i];
             index = i;
         }
-        origin[index] = val;
+        a[index] = val;
     }
 }

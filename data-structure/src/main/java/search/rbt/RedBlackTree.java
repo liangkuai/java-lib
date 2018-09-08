91db57aad01ab3f4242a692d361790b8f0b6ec42
凉快 <751747443@qq.com>
2018-09-09 00:14:52 +0800

    red black tree
    put

--- data-structure/src/main/java/search/rbt/RedBlackTree.java
+++ data-structure/src/main/java/search/rbt/RedBlackTree.java
@@ -11,29 +11,44 @@ public class RedBlackTree<K extends Comparable<K>, V> {
     public Node root;
 
     private class Node {
-        K key;
-        V value;
-        Node left;
-        Node right;
+        private K key;
+        private V value;
+        private Node left;
+        private Node right;
 
         /**
-         * false: 黑节点
-         * true: 红节点
+         * 节点颜色
          */
-        boolean color;
+        private Color color;
 
         public Node(K key, V value) {
             this.key = key;
             this.value = value;
-            this.color = true;
+            this.color = Color.RED;
+            this.left = new Node(Color.BLACK);
+            this.right = new Node(Color.BLACK);
         }
 
-        public Node(K key, V value, boolean color) {
+        public Node(Color color) {
+            this.color = color;
+        }
+
+        public Node(K key, V value, Color color) {
             this.key = key;
             this.value = value;
             this.color = color;
         }
     }
+
+
+    /**
+     * 节点颜色
+     */
+    public enum Color {
+        BLACK,
+        RED
+    }
+
 
     /**
      * 查找
@@ -41,7 +56,6 @@ public class RedBlackTree<K extends Comparable<K>, V> {
      */
     public V get(K key) {
         Node current = root;
-
         while (current != null) {
             int cmp = key.compareTo(current.key);
 
@@ -59,12 +73,12 @@ public class RedBlackTree<K extends Comparable<K>, V> {
 
     public void put(K key, V value) {
         root = put(root, key, value);
-        root.color = false;
+        root.color = Color.BLACK;
     }
 
-    public Node put(Node current, K key, V value) {
+    private Node put(Node current, K key, V value) {
 
-        if (current == null) {
+        if (current == null || current.key == null) {
             return new Node(key, value);
         } else {
             int cmp = key.compareTo(current.key);
@@ -79,18 +93,36 @@ public class RedBlackTree<K extends Comparable<K>, V> {
             }
         }
 
-        if (!current.left.color && current.right.color) {
-            current = leftRotate(current);
-        }
 
-        if (current.left.color && current.left.left.color) {
-            current = rightRotate(current);
-        }
+        if (current.left.color == Color.RED && current.right.color == Color.RED) {
+            // 叔父节点为红色
 
-        if (current.right.color && current.left.color) {
-            current.color = true;
-            current.left.color = false;
-            current.right.color = false;
+            if (current.left.left.color == Color.RED || current.left.right.color == Color.RED
+                    || current.right.left.color == Color.RED || current.right.right.color == Color.RED) {
+                current.color = Color.RED;
+                current.left.color = Color.BLACK;
+                current.right.color = Color.BLACK;
+            }
+        } else if (current.left.color == Color.RED) {
+            // 叔父节点为黑色
+
+            if (current.left.left.color == Color.RED) {
+                current = rightRotate(current);
+            } else
+
+            if (current.left.right.color == Color.RED) {
+                current.left = leftRotate(current.left);
+                current = rightRotate(current);
+            }
+        } else if (current.right.color == Color.RED) {
+            if (current.right.left.color == Color.RED) {
+                current.right = rightRotate(current.right);
+                current = leftRotate(current);
+            } else
+
+            if (current.right.right.color == Color.RED) {
+                current = leftRotate(current);
+            }
         }
 
         return current;
@@ -110,7 +142,7 @@ public class RedBlackTree<K extends Comparable<K>, V> {
 
         // 旋转后，更换颜色
         rightChild.color = current.color;
-        current.color = true;
+        current.color = Color.RED;
 
         return rightChild;
     }
@@ -126,8 +158,25 @@ public class RedBlackTree<K extends Comparable<K>, V> {
 
         // 旋转后，更换颜色
         leftChild.color = current.color;
-        current.color = true;
+        current.color = Color.RED;
 
         return leftChild;
+    }
+
+
+    /**
+     * 中序遍历
+     * 非递归
+     */
+    public void inorderTraversal(RedBlackTree.Node current) {
+        if (current.left != null && current.left.key != null) {
+            inorderTraversal(current.left);
+        }
+
+        System.out.print(current.value);
+
+        if (current.right != null && current.right.key != null) {
+            inorderTraversal(current.right);
+        }
     }
 }

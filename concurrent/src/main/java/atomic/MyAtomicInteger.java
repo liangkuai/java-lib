31d1b15fa19c439450cf0848767ae36321f3abf7
liukai <kai.liu@rutingtech.com>
2018-10-26 22:30:47 +0800

    MyAtomicInteger
    常用操作

--- concurrent/src/main/java/atomic/MyAtomicInteger.java
+++ concurrent/src/main/java/atomic/MyAtomicInteger.java
@@ -31,18 +31,87 @@ public class MyAtomicInteger {
         }
     }
 
+    /**
+     * volatile 保证可见性
+     */
+    private volatile int value;
 
-    private int value;
+
+    public final int get() {
+        return value;
+    }
+
+    public void set(int newValue) {
+        value = newValue;
+    }
 
 
     public MyAtomicInteger() {
     }
 
+    public MyAtomicInteger(int initialValue) {
+        value = initialValue;
+    }
+
+
+    public final int getAndSet(int newValue) {
+        return unsafe.getAndSetInt(this, valueOffset, newValue);
+    }
+
 
     /**
-     * i++（原子操作）
+     * CAS
+     *
+     * @param expect 预期值
+     * @param update 更新值
+     *
+     * @return 是否修改成功
+     */
+    public final boolean compareAndSet(int expect, int update) {
+        return unsafe.compareAndSwapInt(this, valueOffset, expect, update);
+    }
+
+
+    /**
+     * i++
      */
     public int getAndIncrement() {
         return unsafe.getAndAddInt(this, valueOffset, 1);
+    }
+
+    /**
+     * i--
+     */
+    public int getAndDecrement() {
+        return unsafe.getAndAddInt(this, valueOffset, -1);
+    }
+
+    /**
+     * ++i
+     */
+    public int incrementAndGet() {
+        return unsafe.getAndAddInt(this, valueOffset, 1) + 1;
+    }
+
+    /**
+     * --i
+     */
+    public final int decrementAndGet() {
+        return unsafe.getAndAddInt(this, valueOffset, -1) - 1;
+    }
+
+
+    /**
+     * n++
+     */
+    public final int getAndAdd(int delta) {
+        return unsafe.getAndAddInt(this, valueOffset, delta);
+    }
+
+    /**
+     * ++n
+     */
+    public final int addAndGet(int delta) {
+        return unsafe.getAndAddInt(this, valueOffset, delta) + delta;
     }
 }

03125c62a711feeeb14f157816d8716ccb5eb94a
凉快 <751747443@qq.com>
2018-11-17 00:55:17 +0800

    template method pattern

---
+++ design-pattern/src/main/java/behavioral/template_method/Beverage.java
@@ -0,0 +1,30 @@
+package behavioral.template_method;
+
+/**
+ * @author liangkuai
+ * @date 2018/11/16
+ */
+public abstract class Beverage {
+
+    /**
+     * 模板方法
+     */
+    public final void prepareRecipe() {
+        boilWater();
+        brew();
+        pourInCup();
+        addCondiments();
+    }
+
+    private void boilWater() {
+        System.out.println("boilWater");
+    }
+
+    private void pourInCup() {
+        System.out.println("pourInCup");
+    }
+
+    protected abstract void brew();
+
+    protected abstract void addCondiments();
+}

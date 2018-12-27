42395a790a4a123ee7c294fdd13c81d22cce7e69
liukai <liukai@souche.com>
2018-12-27 15:25:04 +0800

    collection
    interface MyMap。基本方法。

--- collection/src/main/java/map/MyMap.java
+++ collection/src/main/java/map/MyMap.java
@@ -7,4 +7,43 @@ package map;
  * @date 2018/12/7
  */
 public interface MyMap<K, V> {
+
+
+    int size();
+
+    boolean isEmpty();
+
+    /**
+     * 查找
+     *
+     * @param key TODO: 为什么参数 key 是 Object 对象
+     */
+    V get(Object key);
+
+
+    /**
+     * 增加
+     */
+    V put(K key, V value);
+
+
+    /**
+     * 删除
+     *
+     * @param key TODO: 为什么参数 key 是 Object 对象
+     */
+    V remove (Object key);
+
+    /**
+     * 清空
+     */
+    void clear();
+
+
+    /**
+     * 是否包含
+     *
+     * @param key TODO: 为什么参数 key 是 Object 对象
+     */
+    boolean containKey(Object key);
 }

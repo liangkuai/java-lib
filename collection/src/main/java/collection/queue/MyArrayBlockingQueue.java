54d042d2f3ba7c6a01b0633a3a950f93e85d0557
liukai <liukai@souche.com>
2019-03-13 18:41:13 +0800

    ArrayBlockingQueue
    offer

--- collection/src/main/java/collection/queue/MyArrayBlockingQueue.java
+++ collection/src/main/java/collection/queue/MyArrayBlockingQueue.java
@@ -1,5 +1,8 @@
 package collection.queue;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @author liukai
  * @date 2019-02-22
@@ -11,10 +14,29 @@ public class MyArrayBlockingQueue<E>
 
     private final Object[] items;
 
+    /**
+     * 入队列的位置
+     */
     private int putIndex;
+
+    /**
+     * 出队列的位置
+     */
     private int takeIndex;
 
     private int count;
+
+
+
+    private final ReentrantLock lock;
+
+    private final Condition notEmpty;
+
+    private final Condition notFull;
+
+
+
+
 
 
     public MyArrayBlockingQueue(int capacity) {
@@ -25,26 +47,53 @@ public class MyArrayBlockingQueue<E>
         if (capacity <= 0)
             throw new IllegalArgumentException();
         this.items = new Object[capacity];
+        lock = new ReentrantLock(fair);
+        notEmpty = lock.newCondition();
+        notFull =  lock.newCondition();
     }
 
 
 
-
-
-
+    /**
+     * 入队列
+     * 队列满，返回 false。
+     *
+     * 入队列的过程被锁定
+     */
     @Override
     public boolean offer(E e) {
+        if (e == null)
+            throw new NullPointerException();
 
-        if (count == items.length) {
-            return false;
-        } else {
-            enqueue(e);
-            return true;
+        lock.lock();
+        try {
+            if (count == items.length) {
+                return false;
+            } else {
+                enqueue(e);
+                return true;
+            }
+        } finally {
+            lock.unlock();
         }
     }
 
-    public void enqueue(E x) {
+    /**
+     * 队列不满时入队列
+     */
+    private void enqueue(E x) {
+        final Object[] items = this.items;
         items[putIndex] = x;
+
+        // 因为队列是基于数组实现，
+        // 每次有元素存入队列时，需要判断是否已经到达数组尾。
+        if (++putIndex == items.length)
+            putIndex = 0;
+        count++;
+
+        // 当队列为空时，尝试从队列中取元素的线程会被阻塞。
+        // 每次有元素存入队列时，唤醒一个阻塞的线程。
+        notEmpty.signal();
     }
 
 
